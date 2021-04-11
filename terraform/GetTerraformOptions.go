5e0ded4c9acf949171ebdeba95c2804d44601eb8
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2021-04-11 10:35:13 +0100

    Removed 'variables' parameter from GetTerraformOptions() method

--- terraform/GetTerraformOptions.go
+++ terraform/GetTerraformOptions.go
@@ -4,12 +4,11 @@ import (
 	"github.com/gruntwork-io/terratest/modules/terraform"
 )
 
-func GetTerraformOptions(terraformDirectoryPath string, variables map[string]interface{}) *terraform.Options {
+func GetTerraformOptions(terraformDirectoryPath string) *terraform.Options {
 
 	terraformOptions := &terraform.Options{
 
 		TerraformDir: terraformDirectoryPath,
-		Vars:         variables,
 		NoColor:      true,
 	}
 
