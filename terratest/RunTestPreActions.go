19152dff5e5e44c4b2f090f3f51c4c5bf4d42f04
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2021-04-11 11:23:23 +0100

    Updated 'RunTestPreActions' to obtain working directory

--- terratest/RunTestPreActions.go
+++ terratest/RunTestPreActions.go
@@ -2,6 +2,7 @@ package terratest
 
 import (
 	"fmt"
+	"os"
 	"testing"
 
 	"github.com/sphenic/go-terratest-helper/terraform"
@@ -12,12 +13,14 @@ func RunTestPreActions(t *testing.T, testFilePath string, terraformVariables map
 	fmt.Printf("\n")
 	fmt.Printf("\n")
 	fmt.Printf("\n")
-	fmt.Printf("Test Script: %s\n", testFilePath)
+	wd, _ := os.Getwd()
+	fmt.Printf("WorkingDir: %s\n", wd)
+	fmt.Printf("TestScript: %s\n", testFilePath)
 	fmt.Printf("\n")
 
 	// Save Terraform variables to file
 	testVariablesFilePath := GetTestVariablesFilePath(terraform.GetTerraformRelativeDirectoryPath(), GetTestVariablesFileName())
-	fmt.Printf("Saving Terraform variables to '%s'...\n", testFilePath)
+	fmt.Printf("Saving Terraform variables to '%s'...\n", testVariablesFilePath)
 	terraform.SaveTerraformVariablesToFile(testVariablesFilePath, terraformVariables)
 
 }
