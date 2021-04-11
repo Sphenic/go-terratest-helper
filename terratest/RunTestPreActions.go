c3d8a36df2dfd4d6baa25ed9e2d25d63f3e4126a
Paul Wilcox <paul.wilcox@sphenic.co.uk>
2021-04-11 12:00:57 +0100

    Updated RunTestPreActions() to remove use of working directory.

--- terratest/RunTestPreActions.go
+++ terratest/RunTestPreActions.go
@@ -2,7 +2,6 @@ package terratest
 
 import (
 	"fmt"
-	"os"
 	"testing"
 
 	"github.com/sphenic/go-terratest-helper/terraform"
@@ -13,14 +12,17 @@ func RunTestPreActions(t *testing.T, testFilePath string, terraformVariables map
 	fmt.Printf("\n")
 	fmt.Printf("\n")
 	fmt.Printf("\n")
-	workingDirectoryPath, _ := os.Getwd()
-	fmt.Printf("WorkingDir: %s\n", workingDirectoryPath)
 	fmt.Printf("TestScript: %s\n", testFilePath)
 	fmt.Printf("\n")
 
+	fmt.Printf("Starting all, pre-test actions...")
+	fmt.Printf("\n")
+
 	// Save Terraform variables to file
-	testVariablesFilePath := GetTestVariablesFilePath(workingDirectoryPath, GetTestVariablesFileName())
+	testVariablesFilePath := GetTestVariablesFilePath(terraform.GetTerraformRelativeDirectoryPath(), GetTestVariablesFileName())
 	fmt.Printf("Saving Terraform variables to '%s'...\n", testVariablesFilePath)
 	terraform.SaveTerraformVariablesToFile(testVariablesFilePath, terraformVariables)
 
+	fmt.Printf("Successfully completed all, pre-test actions.")
+	fmt.Printf("\n")
 }
